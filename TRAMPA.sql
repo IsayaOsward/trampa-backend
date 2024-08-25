d59c6439ab5fd28d0e8a6adcf893483100ee1277
Isaya Osward <ionthefirst97@gmail.com>
2024-08-25 18:17:05 +0300

    Update TRAMPA.sql

--- TRAMPA.sql
+++ TRAMPA.sql
@@ -5,3 +5,16 @@ CREATE TABLE blog_posts (
   images TEXT NOT NULL,   -- Store image filenames as JSON
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `phone_number` varchar(13) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `id` (`id`)
+) 
