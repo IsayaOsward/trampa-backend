5c6d98623eae8f522d64bd5d1ee879f015f42dac
Isaya Osward <ionthefirst97@gmail.com>
2024-08-25 18:57:55 +0300

    Update TRAMPA.sql

--- TRAMPA.sql
+++ TRAMPA.sql
@@ -1,12 +1,3 @@
-CREATE TABLE blog_posts (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  body TEXT NOT NULL,
-  images TEXT NOT NULL,   -- Store image filenames as JSON
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` varchar(100) NOT NULL,
@@ -32,7 +23,23 @@ CREATE TABLE `credentials` (
 	FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE NO ACTION ON DELETE CASCADE
 )
 
+CREATE TABLE blog_posts (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  images TEXT NOT NULL,   -- Store image filenames as JSON
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE `register_events` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`event_id` INT(10) NOT NULL,
+	`title` VARCHAR(300) NOT NULL,
+	`theme` VARCHAR(200) NOT NULL,
+	`venue` VARCHAR(200) NOT NULL,
+	`date` VARCHAR(200) NOT NULL,
+	PRIMARY KEY (`event_id`),
+	INDEX `id` (`id`))
 
 CREATE TABLE `event_registration` (
 	`id` INT(10) NOT NULL AUTO_INCREMENT,
@@ -46,5 +53,17 @@ CREATE TABLE `event_registration` (
 	`telephone` VARCHAR(50) NOT NULL,
 	`country` VARCHAR(50) NOT NULL,
 	`membership_number` VARCHAR(50) NOT NULL,
-	INDEX `id` (`id`) USING BTREE
+	INDEX `id` (`id`)
+	CONSTRAINT `FK_events_regitser_events` FOREIGN KEY (`event_id`) REFERENCES `register_events` (`event_id`) ON UPDATE CASCADE ON DELETE CASCADE
 )
+
+
+CREATE TABLE `membership` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`user_id` VARCHAR(70) NOT NULL,
+	`membership_id` VARCHAR(70),
+	PRIMARY KEY (`membership_id`),
+	INDEX `id` (`id`),
+	INDEX `FK_membership_users` (`user_id`),
+	CONSTRAINT `FK_membership_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
+);
